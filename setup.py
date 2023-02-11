f3e800de3f5a77dbd82046775ee51127eb106cc3
xloc <xloc.cc@outlook.com>
2023-02-11 14:34:06 -0800

    make pip installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='genshin-artifact-scanner',
+    version='0.0.1',
+    author='xloc',
+    author_email='xloc.cc@outlook.com',
+    description='An artifact parser from the artifact listing pages to a list of artifact attributes for the game Genshin Impact',
+    packages=find_packages(),
+)
