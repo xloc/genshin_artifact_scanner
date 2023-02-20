4a16aeae1754177a6e97b8b9dd58fda2ef6ed0ee
xloc <xloc.cc@outlook.com>
2023-02-20 03:15:34 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ setup(
     author_email='xloc.cc@outlook.com',
     description='An artifact parser from the artifact listing pages to a list of artifact attributes for the game Genshin Impact',
     packages=find_packages(),
+    py_modules=['sketch'],
 )
